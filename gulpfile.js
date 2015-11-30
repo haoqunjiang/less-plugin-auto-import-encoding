281e64c87589231486141a3ed13d13b12b97baf7
sodatea <sodazju@gmail.com>
2015-11-30 12:21:20 +0800

    Fix typescript building

--- gulpfile.js
+++ gulpfile.js
@@ -5,46 +5,38 @@ const path = require('path');
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const babel = require('gulp-babel');
+const tslint = require('gulp-tslint');
 const del = require('del');
 // gulp-sourcemaps
 
-const tsOutputPath = './release';
+const tsConfig = require('./tsConfig.json');
+const tsFilesGlob = tsConfig.filesGlob || tsConfig.files || 'lib/**/*.ts';
 
 gulp.task('default', ['build']);
 
-gulp.task('watch', ['ts-babel'], () => {
-    gulp.watch('lib/*.ts', ['build']);
-});
+gulp.task('watch', ['build'], () =>
+    gulp.watch('lib/*.ts', ['build'])
+);
 
-gulp.task('build', ['clean', 'compile-ts-babel', 'gen-ts-refs']);
+gulp.task('build', ['compile-ts-babel']);
 
-
-gulp.task('clean', (cb) => {
-    del(tsOutputPath, cb);
-});
-
-gulp.task('compile-ts-babel', () => {
+gulp.task('compile-ts-babel', ['clean'], () => {
     // create a project first, for incremental compilation
     const tsProject = ts.createProject(path.join(__dirname, 'tsconfig.json'));
     const babelConfig = require('./package.json').babel;
 
-    return gulp.src('lib/**/*.ts', { base: './lib' })  // base served as rootDir in tsconfig.json
+    return gulp.src(tsFilesGlob, { base: './lib' })  // base served as rootDir in tsconfig.json
         .pipe(ts(tsProject))
         .pipe(babel(babelConfig))
-        .pipe(gulp.dest(tsOutputPath));
+        .pipe(gulp.dest(tsConfig.compilerOptions.outDir));
 });
 
-gulp.task('gen-ts-refs', () => {
-    return;
-});
+gulp.task('clean', () =>
+    del(tsConfig.compilerOptions.outDir)
+);
 
-gulp.task('tslint', () => {
-    return;
-});
-
-// this is an opt-in task.
-// since eslint only validates flow, and any syntax similarity between flow & typescript is pure coincidence.
-// so there might be some unnecessary error reports.
-gulp.task('eslint', () => {
-    return;
-});
+gulp.task('tslint', () =>
+    gulp.src(tsFilesGlob)
+        .pipe(tslint())
+        .pipe(tslint.report('verbose'))
+);
--- lib/__tests__/index.spec.ts
+++ lib/__tests__/index.spec.ts
@@ -1,15 +1,16 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
-import test from 'tape';
+import test from 'blue-tape';
 
 function sleep(ms: number): Promise<number> {
-  return new Promise<number>(resolve => setTimeout(resolve, ms));
+    return new Promise((resolve: (value: any) => void) =>
+        setTimeout(resolve, ms)
+    );
 }
 
-test('sleep', async function(t) {
+test('sleep', async function(t: test.Test): Promise<void> {
     const start: number = Date.now();
     await sleep(20);
     const end: number = Date.now();
     t.ok(end - start >= 20, 'takes about 20 milliseconds');
-    t.end();
 });
