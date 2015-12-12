3f50ee4a7ed110f039ae5abd39ab25c1b40bd424
sodatea <sodazju@gmail.com>
2015-12-13 00:53:15 +0800

    Export a factory function

--- lib/import-encoding-fix.ts
+++ lib/import-encoding-fix.ts
@@ -3,40 +3,55 @@
 import * as less from 'less';
 
 export interface PluginOptions {
-    preferEncoding: string;
+    preferEncoding?: string;
 };
 
-export class ImportEncodingFix extends less.FileManager {
-    options: PluginOptions;
+export function createImportEncodingFix(less: LessStatic): typeof Less.FileManager {
+    class ImportEncodingFix extends less.FileManager {
+        options: PluginOptions;
 
-    constructor(opts: PluginOptions) {
-        super();
-        this.options = opts;
+        constructor(opts: PluginOptions = {}) {
+            super();
+            this.options = opts;
+        }
+
+        supports(): boolean {
+            return true;
+        }
+
+        supportsSync(): boolean {
+            return true;
+        }
+
+        loadFile(
+            filename: string,
+            currentDirectory: string,
+            options: Less.LoadOptions = {},
+            environment: any,
+            callback: (err: void|Less.LessError, data: Less.LoadedFile) => void
+            ): Promise<Less.LoadedFile> {
+/*            let data: Less.LoadError|Less.LoadedFile;
+
+            if (options.syncImport) {
+                data = this.loadFileSync(filename, currentDirectory, options, environment);
+                return callback(data.error, data);
+            }
+*/
+            throw new Error('async load has not been implemented yet');
+        };
+
+        loadFileSync(
+            filename: string,
+            currentDirectory: string,
+            options: Less.LoadOptions = {},
+            environment: any
+            ): Less.LoadError|Less.LoadedFile {
+            return {
+                contents: '',
+                filename: ''
+            };
+        };
     }
 
-    supports() {}
-
-    supportsSync() {}
-
-    loadFile(
-        filename: string,
-        currentDirectory: string,
-        options: any,
-        environment: any,
-        callback: (err: void|Less.LessError, data: Less.LoadedFile) => void
-        ): Promise<Less.LoadedFile> {
-            throw new Error('todo');
-        };
-
-    loadFileSync(
-        filename: string,
-        currentDirectory: string,
-        options: any,
-        environment: any
-        ): Less.LoadError|Less.LoadedFile {
-        return {
-            contents: '',
-            filename: ''
-        };
-    };
+    return ImportEncodingFix;
 }
