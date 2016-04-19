8be9e047e9a67f0f8accfba1cd15430ba3db881c
sodatea <sodazju@gmail.com>
2016-04-19 19:32:45 +0800

    Enable allowSyntheticDefaultImports flag

--- typings/blue-tape/blue-tape.d.ts
+++ typings/blue-tape/blue-tape.d.ts
@@ -2,8 +2,5 @@
 
 declare module 'blue-tape' {
 	import tape = require('tape');
-	// should use commonjs export
-	// but for babel interop purpose, I have to modify it to export default ... sytanx
-	// this is ugly
-	export default tape;
+	export = tape;
 }
